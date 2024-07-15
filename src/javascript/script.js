9bff800bc564c6a152e4c9ae2a267d0beed542e6
lorransantosdev <lorransantossarmentol@gmail.com>
2024-07-15 15:54:10 -0300

    teste da page answer

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -20,9 +20,22 @@ $(document).ready(function() {
         duration: 1500,
         distance: '15%'
     });
-
-
 });
+
+function toggleAnswer(element) {
+    const answer = element.nextElementSibling;
+    const icon = element.querySelector('i');
+    
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+      icon.classList.remove('fa-minus');
+      icon.classList.add('fa-plus');
+    } else {
+      answer.style.display = "block";
+      icon.classList.remove('fa-plus');
+      icon.classList.add('fa-minus');
+    }
+  }
 
 
 
